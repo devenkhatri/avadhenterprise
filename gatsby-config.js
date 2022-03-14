ad8e474c56fb467c112bab5b7c66ab82eb573db7
Harshna Goratela <harshna.goratela@gmail.com>
2022-03-14 08:18:00 +0530

    First change

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,10 +6,10 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://gatsbycontentfulhomepage.gatsbyjs.io/",
-    title: "Gatsby Contentful Homepage Starter",
-    author: `Gatsby`,
-    description: "A Gatsby Starter for building homepages with Contentful",
+    siteUrl: "https://avadhenterprise.gatsbyjs.io/",
+    title: "Avadh Enterprise",
+    author: `Deven Goratela`,
+    description: "Avadh Enterprise - A Service Company",
   },
   plugins: [
     {
--- src/colors.css.ts
+++ src/colors.css.ts
@@ -1,8 +1,8 @@
 export const colors = {
-  background: "#ffd500",
-  text: "#005bbb",
-  primary: "#005bbb",
-  muted: "#f5cc00",
-  active: "#004287",
+  background: "#eff6ff",
+  text: "#004ca3",
+  primary: "#004ca3",
+  muted: "#e6f1ff",
+  active: "#001d3d",
   black: "#000",
 }
